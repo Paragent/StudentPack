f8686c77534f2063d480e3c8f518efbe6f14311a
Alexander Kulazhkin <skulazhkin126@gmail.com>
2024-01-08 15:19:29 +1000

    Stage 2

--- studPack.cpp
+++ studPack.cpp
@@ -27,8 +27,28 @@ class Student{
         }
 };
 
+class Teacher
+{
+    private:
+        string subject;
+
+    public:
+        Teacher(string NewSubject) { this->subject = NewSubject; }
+        void giveGrade(Student& student, int grade){
+            student.assignGrade(grade);
+        }
+
+        string getSubject(){
+            return this->subject;
+        }
+};
+
 int main()
 {
+    Teacher t1("OOP");
+    Teacher t2("Math");
+    Teacher t3("English");
+
     Student s1("Alex", "Mercer");
     Student s2("James", "Franko");
     Student s3("Peter", "Parker");
@@ -45,8 +65,25 @@ int main()
     s3.assignGrade(4);
     s3.assignGrade(4);
 
+    t1.giveGrade(s1, 5);
+    t1.giveGrade(s2, 4);
+    t1.giveGrade(s3, 2);
+
+    t1.giveGrade(s1, 3);
+    t1.giveGrade(s2, 3);
+    t1.giveGrade(s3, 3);
+
+    t3.giveGrade(s1, 2);
+    t3.giveGrade(s2, 2);
+    t3.giveGrade(s3, 2);
+
     cout << (s1.isExcellentStudent()?"Excellent":"Not excellent") << endl;
     cout << (s2.isExcellentStudent()?"Excellent":"Not excellent") << endl;
-    cout << (s3.isExcellentStudent()?"Excellent":"Not excellent");
+    cout << (s3.isExcellentStudent()?"Excellent":"Not excellent") << endl;
+    
+    cout << t1.getSubject() << endl;
+    cout << t2.getSubject() << endl;
+    cout << t3.getSubject() << endl;
+
     return 0;
 }
